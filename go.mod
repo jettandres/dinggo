524b4a0e0cd3652aeae7c2ade1cb4215cdc6c84e
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-07-29 09:05:23 +0800

    Setup client with auth and print notif

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/jettandres/dinggo
 
 go 1.18
+
+require (
+	github.com/google/go-github/v45 v45.2.0
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/go-github/v45/github"
+	"golang.org/x/oauth2"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello dinggo")
+	loadEnv()
+	githubToken := os.Getenv("GITHUB_TOKEN")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	notifs, resp, err := client.Activity.ListNotifications(ctx, &github.NotificationListOptions{
+		All: true,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("hello dinggo: %s\n", resp.Status)
+
+	for _, notif := range notifs {
+		fmt.Println(notif.Subject.GetTitle())
+	}
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 }
